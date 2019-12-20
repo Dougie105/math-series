c2efc1b772f4727fe1dae84c5540837cb1551091
Terrell Douglas <terrelloydouglas@yahoo.com>
2019-12-20 12:43:15 -0800

    tests done and code fixed

--- series.py
+++ series.py
@@ -1,13 +1,18 @@
 # Fibonacci Function
 
 def fibonacci(n):
-  if n <= 1:
-    return n
-  return fibonacci(n-1) + fibonacci(n-2)
+  '''The function should have one parameter n. The function should return the nth value in the fibonacci series using recursion or iteration.'''
+  if n == 0:
+    return 0
+  elif n == 1:
+    return 1
+  else:
+    return fibonacci(n-1) + fibonacci(n-2)
 
 # Lucas Function
 
 def lucas(r):
+  '''Returns the nth value in the lucas numbers Again, you may use recursion or iteration'''
   if r == 0:
     return 2
   if r == 1:
@@ -17,7 +22,8 @@ def lucas(r):
 # Sum Series Function
 
 def sum_series(x, y=0, z=1):
-  if x == 2:
+  '''Function with one required parameter and two optional parameters. The required parameter will determine which element in the series to print. The two optional parameters will have default values of 0 and 1 and will determine the first two values for the series to be produced.'''
+  if x == 0:
     return y
   if x == 1:
     return z
